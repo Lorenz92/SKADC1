a6f1d1968051d4b3a18e81a80b8006325929b3c4
Lorenzo Cellini <lorenzo.cellini5@gmail.com>
2021-05-03 19:45:50 +0200

    Loaded VGG16 weights into our model.

--- src/config.py
+++ src/config.py
@@ -9,6 +9,7 @@ DATA_FOLDER = os.path.join(os.getcwd(), "data")
 TRAIN_DATA_FOLDER = os.path.join(DATA_FOLDER, "training")
 TRAIN_PATCHES_FOLDER = os.path.join(TRAIN_DATA_FOLDER, "patches")
 TRAIN_SET_PATH = os.path.join(TRAIN_DATA_FOLDER, "B1_training_clean_SKA.txt") #TODO: fix this
+MODEL_WEIGHTS = os.path.join(os.getcwd(), "model")
 
 required_files = [
     {
