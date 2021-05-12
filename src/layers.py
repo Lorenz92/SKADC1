95602bb3af6ed409f96d72c3735715e2dd6723e0
Lorenzo Cellini <lorenzo.cellini5@gmail.com>
2021-05-12 19:40:39 +0200

    Created vgg16 class models.

--- src/layers.py
+++ src/layers.py
@@ -1,10 +1,31 @@
 # First net version
 import keras.backend as K
-from keras.layers import Input, Conv2D, MaxPooling2D, Layer
+import tensorflow as tf
+from keras.layers import Conv2D, MaxPooling2D, Layer, Flatten, TimeDistributed, Dense, Dropout
 
 # ANCHOR_NUM = 30
 # PROB_PRED_OUT = 2 * ANCHOR_NUM
 # COORD_PRED_OUT = 4 * ANCHOR_NUM 
+
+
+# def custom_input_layer(input_shape, channels=3, kernel_size=(1, 1), padding='same', activation='relu', name='Custom_input_layer'):
+
+#     print(input_shape)
+
+#     inp = Input(input_shape)
+#     x = Conv2D(filters=channels, kernel_size=kernel_size, padding=padding, activation=activation, name=name)(inp)
+
+#     return inp, x
+
+class Expander(Layer):
+    def __init__(self, channels=3, kernel_size=(1, 1), padding='same', activation='relu', name='Custom_input_layer'):
+        super(Expander, self).__init__()
+        self.conv2d = Conv2D(filters=channels, kernel_size=kernel_size, padding=padding, activation=activation, name=name) #TODO: parametrizzare tutto
+
+    def call(self, inputs):
+        x = self.conv2d(inputs)
+        return x
+
 
 def vgg16(input_image):
 
@@ -107,23 +128,116 @@ class RpnNet(Layer):
         return [cls_pred, reg_pred]
 
 
+class RoiPoolingConv(Layer):
+    '''ROI pooling layer for 2D inputs.
+    See Spatial Pyramid Pooling in Deep Convolutional Networks for Visual Recognition,
+    K. He, X. Zhang, S. Ren, J. Sun
+    # Arguments
+        pool_size: int
+            Size of pooling region to use. pool_size = 7 will result in a 7x7 region.
+        num_rois: number of regions of interest to be used
+    # Input shape
+        list of two 4D tensors [X_img,X_roi] with shape:
+        X_img:
+        `(1, rows, cols, channels)`
+        X_roi:
+        `(1,num_rois,4)` list of rois, with ordering (x,y,w,h)
+    # Output shape
+        3D tensor with shape:
+        `(1, num_rois, channels, pool_size, pool_size)`
+    '''
+    def __init__(self, pool_size, num_rois, **kwargs):
 
-# def custom_input_layer(input_shape, channels=3, kernel_size=(1, 1), padding='same', activation='relu', name='Custom_input_layer'):
+        # self.dim_ordering = K.common.image_dim_ordering()
+        self.pool_size = pool_size
+        self.num_rois = num_rois
 
-#     print(input_shape)
+        super(RoiPoolingConv, self).__init__(**kwargs)
 
-#     inp = Input(input_shape)
-#     x = Conv2D(filters=channels, kernel_size=kernel_size, padding=padding, activation=activation, name=name)(inp)
+    def build(self, input_shape):
+        self.channels = input_shape[0][3]   
 
-#     return inp, x
+    def compute_output_shape(self, input_shape):
+        return None, self.num_rois, self.pool_size, self.pool_size, self.channels
+
+    def call(self, x, mask=None):
+
+        assert(len(x) == 2)
+
+        # x[0] is image with shape (rows, cols, channels)
+        img = x[0]
+
+        # x[1] is roi with shape (num_rois,4) with ordering (x,y,w,h)
+        rois = x[1]
+
+        input_shape = K.shape(img)
+
+        outputs = []
+
+        for roi_idx in range(self.num_rois):
+
+            x = rois[0, roi_idx, 0]
+            y = rois[0, roi_idx, 1]
+            w = rois[0, roi_idx, 2]
+            h = rois[0, roi_idx, 3]
+
+            x = K.cast(x, 'int32')
+            y = K.cast(y, 'int32')
+            w = K.cast(w, 'int32')
+            h = K.cast(h, 'int32')
+
+            # Resized roi of the image to pooling size (7x7)
+            rs = tf.image.resize(img[:, y:y+h, x:x+w, :], (self.pool_size, self.pool_size))
+            outputs.append(rs)
+                
+
+        final_output = K.concatenate(outputs, axis=0)
+
+        # Reshape to (1, num_rois, pool_size, pool_size, channels)
+        # Might be (1, 4, 7, 7, 3)
+        final_output = K.reshape(final_output, (1, self.num_rois, self.pool_size, self.pool_size, self.channels))
+
+        # permute_dimensions is similar to transpose
+        final_output = K.permute_dimensions(final_output, (0, 1, 2, 3, 4))
+
+        return final_output
+    
+    
+    def get_config(self):
+        config = {'pool_size': self.pool_size,
+                  'num_rois': self.num_rois}
+        base_config = super(RoiPoolingConv, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
 
 
+class Classifier(Layer):
 
-class Expander(Layer):
-    def __init__(self, channels=3, kernel_size=(1, 1), padding='same', activation='relu', name='Custom_input_layer'):
-        super(Expander, self).__init__()
-        self.conv2d = Conv2D(filters=channels, kernel_size=kernel_size, padding=padding, activation=activation, name=name) #TODO: parametrizzare tutto
+    def __init__(self, num_classes, **kwargs):
+        super(Classifier, self).__init__()
+
+        self.num_classes = num_classes
+        
+        self.td_flatten = TimeDistributed(Flatten(name='flatten'))
+        self.td_fc_1 = TimeDistributed(Dense(4096, activation='relu', name='fc1'))
+        self.td_do_1 = TimeDistributed(Dropout(0.5))
+        self.td_fc_2 = TimeDistributed(Dense(4096, activation='relu', name='fc2'))
+        self.td_do_2 = TimeDistributed(Dropout(0.5))
+
+        self.td_fc_cls = TimeDistributed(Dense(self.num_classes, activation='softmax', kernel_initializer='zero'), name='dense_class_{}'.format(self.num_classes))
+        self.td_fc_reg = TimeDistributed(Dense(4 * (self.num_classes), activation='linear', kernel_initializer='zero'), name='dense_regress_{}'.format(self.num_classes))
 
     def call(self, inputs):
-        x = self.conv2d(inputs)
-        return x
+
+        x = self.td_flatten(inputs)
+        x = self.td_fc_1(x)
+        x = self.td_do_1(x)
+        x = self.td_fc_2(x)
+        x = self.td_do_2(x)
+
+        # There are two output layer
+        # out_class: softmax acivation function for classify the class name of the object
+        # out_regr: linear activation function for bboxes coordinates regression
+        out_class = self.td_fc_cls(x)
+        out_regr = self.td_fc_reg(x)
+        
+        return [out_class, out_regr]
--- src/models.py
+++ src/models.py
@@ -1,15 +1,15 @@
 import keras
 from keras.layers import Input
-from src.layers import Expander, vgg16, RpnNet
+from src.layers import Expander, vgg16, RpnNet, RoiPoolingConv, Classifier
 
 
-class VGG16(keras.Model):
+class RpnVgg16(keras.Model):
     """
     VGG16 model wrapper
     """
 
     def __init__(self, anchor_num=30, **kwargs):
-        # super(VGG16, self).__init__()
+        # super(RpnVgg16, self).__init__()
         self.anchor_num = anchor_num
         self.build(**kwargs)
     
@@ -22,14 +22,14 @@ class VGG16(keras.Model):
         x = vgg16(x)
 
         # Create Region Proposal Net
-        x = RpnNet(self.anchor_num)(x)
+        rpn_cls, rpn_reg = RpnNet(self.anchor_num)(x)
 
         if training:
             print('TRAINING') #training loop
         else:
             print('TEST')
 
-        return x
+        return [rpn_cls, rpn_reg]
 
     def build(self, shape, **kwargs):
         """
@@ -38,4 +38,91 @@ class VGG16(keras.Model):
         # Replace with shape=[None, None, None, 1] if input_shape is unknown.
         inputs  = Input(shape=shape)
         outputs = self.call(inputs)
-        super(VGG16, self).__init__(name="VGG16", inputs=inputs, outputs=outputs, **kwargs) 
+        super(RpnVgg16, self).__init__(name="RpnVgg16", inputs=inputs, outputs=outputs, **kwargs) 
+
+
+class ClsVgg16(keras.Model):
+    """
+    VGG16 model wrapper
+    """
+
+    def __init__(self, num_classes, **kwargs):
+        # super(ClsVgg16, self).__init__()
+        self.pooling_regions = 7
+        self.num_rois = 4
+        self.num_classes = num_classes
+        self.build(**kwargs)
+    
+    def call(self, inputs, training=False):
+        
+        # Expand image channels from 1 to 3        
+        x = Expander()(inputs[0])
+
+        # Feature extraction network
+        x = vgg16(x)
+        
+        # Final classification and regression step
+        x = RoiPoolingConv(self.pooling_regions, self.num_rois)([x, inputs[1]])
+        x = Classifier(self.num_classes)(x)
+
+        if training:
+            print('TRAINING') #training loop
+        else:
+            print('TEST')
+
+        return x
+
+    def build(self, shape_1, shape_2=(None, 4), **kwargs):
+        """
+        Replicates Model(inputs=[inputs], outputs=[outputs]) of functional model.
+        """
+        # Replace with shape=[None, None, None, 1] if input_shape is unknown.
+        inputs  = [Input(shape=shape_1), Input(shape=shape_2)]
+        outputs = self.call(inputs)
+        super(ClsVgg16, self).__init__(name="ClsVgg16", inputs=inputs, outputs=outputs, **kwargs)
+
+
+class TotalVgg16(keras.Model):
+    """
+    This model is the combination of RpnVgg16 and ClsVgg16, needed to save and load weights by name
+    """
+
+    def __init__(self, num_classes, anchor_num=30, **kwargs):
+        # super(ClsVgg16, self).__init__()
+        self.anchor_num = anchor_num
+        self.pooling_regions = 7
+        self.num_rois = 4
+        self.num_classes = num_classes
+        self.build(**kwargs)
+    
+    def call(self, inputs, training=False):
+        
+        # Expand image channels from 1 to 3        
+        x = Expander()(inputs[0])
+
+        # Feature extraction network
+        x = vgg16(x)
+
+        # Region Proposal Net
+        rpn_cls, rpn_reg = RpnNet(self.anchor_num)(x)
+        
+        # Final classification and regression step
+        x = RoiPoolingConv(self.pooling_regions, self.num_rois)([x, inputs[1]])
+        x = Classifier(self.num_classes)(x)
+
+        if training:
+            print('TRAINING') #training loop
+        else:
+            print('TEST')
+
+        return [rpn_cls, rpn_reg, x]
+
+    def build(self, shape_1, shape_2=(None, 4), **kwargs):
+        """
+        Replicates Model(inputs=[inputs], outputs=[outputs]) of functional model.
+        """
+        # Replace with shape=[None, None, None, 1] if input_shape is unknown.
+        inputs  = [Input(shape=shape_1), Input(shape=shape_2)]
+        outputs = self.call(inputs)
+        super(TotalVgg16, self).__init__(name="ClsVgg16", inputs=inputs, outputs=outputs, **kwargs)
+
