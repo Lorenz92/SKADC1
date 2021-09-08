Fixed map computation + added validation step at epoch's end.

src/dataset.py
@@ -807,7 +807,7 @@ class SKADataset:
         plt.show()
         return
 
-    def split_train_val(self, random_state, val_portion=.2, balanced=False):
+    def split_train_val(self, random_state, val_portion=.2, balanced=False, size=None):
         if not balanced:
             patch_list = self.patch_list
         else:
@@ -815,6 +815,8 @@ class SKADataset:
             assert len(self.balanced_patch_list) > 1, 'Attention! There is no balanced_patch_list available!'
             patch_list = self.balanced_patch_list
             print(len(patch_list))
+
+        patch_list = np.random.choice(patch_list, size, replace=False)
 
         train, val = train_test_split(patch_list, test_size=val_portion, random_state=random_state)
         print(f'Train list consists of {len(train)} patches')
@@ -865,15 +867,19 @@ class SKADataset:
 
         for key, _ in less_freq_classes.items():
             repeated_patches = []
-            print('minor class:', self.patches_dict[key])
+            # print('minor class:', key, self.patches_dict[key])
             patch_to_be_repeated = np.setdiff1d(self.patches_dict[key], self.patches_dict[most_frequent_class])
-            print('patch_to_be_repeated:', patch_to_be_repeated)
+            # print('patch_to_be_repeated:', patch_to_be_repeated)
+            if len(patch_to_be_repeated)==0:
+                patch_to_be_repeated = self.patches_dict[key]
             try:
                 ratio = max_freq // len(patch_to_be_repeated)
                 repeated_patches = np.tile(patch_to_be_repeated, ratio).tolist()
             except:
                 continue
             balanced_patch_list += repeated_patches
+
+        # np.random.sample(balanced_patch_list, size)
         
 
         return balanced_patch_list

src/train.py
@@ -2,20 +2,20 @@ import time
 import os
 import numpy as np
 
+from keras.utils import generic_utils
+import tensorflow as tf
+
 import src.preprocessing as prep
 import src.config as config
 import src.utils as utils
-
-from keras.utils import generic_utils
-import tensorflow as tf
-import sys
+import src.models as models
+import src.losses as loss
 
 np.random.seed(config.RANDOM_SEED)
 
-
 # Training loop
 
-def train_frcnn(rpn_model, detector_model, total_model, train_patch_list, class_list, num_epochs, patches_folder_path, backbone, pixel_mean=None, resume_train=True):
+def train_frcnn(rpn_model, detector_model, total_model, train_patch_list, rpn_model_eval, detector_model_eval, total_model_eval, val_patch_list, class_list, num_epochs, patches_folder_path, backbone, pixel_mean=None, resume_train=True):
 
     ######### build class_mapping
 
@@ -29,12 +29,13 @@ def train_frcnn(rpn_model, detector_model, total_model, train_patch_list, class_
     train_datagen = prep.get_anchor_gt(patches_folder_path, train_patch_list, backbone, pixel_mean=pixel_mean)
 
     iter_num = 0
-    epoch_length = 100
+    epoch_length = 2
     rpn_accuracy_rpn_monitor = []
     rpn_accuracy_for_epoch = []
     start_time = time.time()
     num_epochs = num_epochs
     losses = np.zeros((epoch_length, 5))
+    scores = np.zeros((epoch_length, 2))
 
 
     ######### resume training
@@ -42,12 +43,17 @@ def train_frcnn(rpn_model, detector_model, total_model, train_patch_list, class_
     if resume_train:
         previous_losses = np.load(f"{config.MODEL_WEIGHTS}/{backbone}/loss_history.npy")
         best_loss = min(previous_losses[:,:-1].sum(axis=1))
-        print(f'\nPrevious best loss: {best_loss}')
         del previous_losses
+
+        previous_scores = np.load(f"{config.MODEL_WEIGHTS}/{backbone}/scores_history.npy")
+        best_mAP = max(previous_scores[:,0])
+        print(f'\nPrevious best loss: {best_loss} - best mAP: {best_mAP}')
+        del previous_scores
     else:
-        if os.path.exists(f"{config.MODEL_WEIGHTS}/{backbone}/loss_history.npy"):
-            raise ValueError('There is already a loss history file. Please delete it first.')
+        if (os.path.exists(f"{config.MODEL_WEIGHTS}/{backbone}/loss_history.npy") or os.path.exists(f"{config.MODEL_WEIGHTS}/{backbone}/scores_history.npy")):
+            raise ValueError('There is already a loss history or a score history file. Please delete it first.')
         best_loss = np.Inf
+        best_mAP = 0.
 
     ######### (re-)start training
     for epoch in range(num_epochs):
@@ -219,11 +225,32 @@ def train_frcnn(rpn_model, detector_model, total_model, train_patch_list, class_
                     else:
                         loss_hist = losses_to_save
                         counter = 1
-                        resume_train = True
-
+                    print('saving loss')
                     np.save(f"{config.MODEL_WEIGHTS}/{backbone}/loss_history.npy", loss_hist)
                     total_model.save_weights(f'{config.MODEL_WEIGHTS}/{backbone}/0_frcnn_{backbone}.h5')
+                    
+                    print('Validating model on validation set...')
 
+                    models.load_weigths(rpn_model_eval, detector_model_eval, backbone, checkpoint=f'0_frcnn_{backbone}.h5')
+                    models.compile_models(rpn_model_eval, detector_model_eval, total_model_eval, rpn_losses=[loss.rpn_loss_cls, loss.rpn_loss_regr], detector_losses=[loss.detector_loss_cls, loss.detector_loss_regr], class_list=class_list)
+
+
+                    _, mAP, mPrecision = utils.evaluate_model(rpn_model_eval, detector_model_eval, backbone, val_patch_list, class_list, map_threshold=.5, acceptance_treshold=.5)
+
+                    if (mPrecision == 0. and mAP == 1.):
+                        mAP=0.
+                    
+                    scores_to_save = [[mAP, mPrecision]]
+                    print(scores_to_save)
+
+                    if resume_train:
+                        previous_scores = np.load(f"{config.MODEL_WEIGHTS}/{backbone}/scores_history.npy")
+                        scores_hist = np.concatenate((previous_scores, scores_to_save), axis=0)
+                    else:
+                        scores_hist = scores_to_save
+                        resume_train = True
+                    
+                    np.save(f"{config.MODEL_WEIGHTS}/{backbone}/scores_history.npy", scores_hist)
 
                     iter_num = 0
 
@@ -231,7 +258,11 @@ def train_frcnn(rpn_model, detector_model, total_model, train_patch_list, class_
                     if curr_loss < best_loss:
                         print('Total loss decreased from {} to {}, saving weights'.format(best_loss,curr_loss))
                         best_loss = curr_loss
-                        total_model.save_weights(f'{config.MODEL_WEIGHTS}/{backbone}/{counter}_frcnn_{backbone}.h5')
+                        total_model.save_weights(f'{config.MODEL_WEIGHTS}/{backbone}/loss_{counter}_frcnn_{backbone}.h5')
+                    if (mAP > best_mAP and mPrecision > 0):
+                        print('mAP decreased from {} to {}, saving weights'.format(best_mAP,mAP))
+                        best_mAP = mAP
+                        total_model.save_weights(f'{config.MODEL_WEIGHTS}/{backbone}/map_{counter}_frcnn_{backbone}.h5')
                     
                     start_time = time.time()
                     break

src/utils.py
@@ -543,6 +543,19 @@ def plot_loss(history):
     
     return
 
+def plot_scores(history):
+    r_epochs = history.shape[0]
+
+    plt.figure(figsize=(15,5))
+    plt.subplot(1,2,1)
+    plt.plot(np.arange(0, r_epochs), history[:,0], 'r')
+    plt.title('mAP_0.5')
+    plt.subplot(1,2,2)
+    plt.plot(np.arange(0, r_epochs), history[:,1], 'r')
+    plt.title('macro Precision')
+    plt.show()
+
+
 def get_real_coordinates(x1, y1, x2, y2):
     ratio = config.resizeFinalDim / config.patch_dim if config.resizePatch else 1.
     real_x1 = int(round(x1 // ratio))
@@ -562,7 +575,7 @@ def get_detections(patch_id, bboxes, probs):
        
         for jk in range(new_boxes.shape[0]):
             (x1, y1, x2, y2) = new_boxes[jk,:]
-            print((x1, y1, x2, y2))
+            # print((x1, y1, x2, y2))
             (real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(x1, y1, x2, y2)
 
             boxes_coords['x1s'].append(real_x1)
@@ -584,15 +597,15 @@ def get_predictions(image, class_list, acceptance_treshold, rpn_model, detector_
     class_mapping = {key:value for key, value in enumerate(class_list)}
     class_mapping[len(class_mapping)] = 'bg'
 
-    print(class_mapping)
+    # print(class_mapping)
 
-    print('Predict')
+    # print('Predict')
 
     # get the feature maps and output from the RPN
     [Y1, Y2, F] = rpn_model.predict_on_batch(image)
     # [Y1, Y2] = rpn_model.predict_on_batch(image)
 
-    print('rpn_to_roi')
+    # print('rpn_to_roi')
 
     # print(Y1.shape, Y2.shape)
 
@@ -622,9 +635,9 @@ def get_predictions(image, class_list, acceptance_treshold, rpn_model, detector_
         # print(ROIs)
 
         [P_cls, P_regr] = detector_model.predict([F, ROIs])
-        # [P_cls, P_regr] = detector_model.predict([image, ROIs])
-        print(P_cls)
-        print(P_regr)
+        
+        # print(P_cls)
+        # print(P_regr)
         for ii in range(P_cls.shape[1]):
             # if classification perc is too low OR it is. a 'bg' image THEN discard
             if np.max(P_cls[0,ii,:]) < acceptance_treshold or np.argmax(P_cls[0,ii,:]) == (P_cls.shape[2] - 1):
@@ -660,8 +673,8 @@ def get_predictions(image, class_list, acceptance_treshold, rpn_model, detector_
                 
                 [[[x]], [[y]], [[w]], [[h]]] = apply_regr_np(X, T)
 
-                # if w==0 or h==0 or x <0 or y <0:
-                #     continue
+                if w==0 or h==0 or x <0 or y <0:
+                    continue
 
             except Exception as e:
                 print('Exception: {}'.format(e))
@@ -670,47 +683,43 @@ def get_predictions(image, class_list, acceptance_treshold, rpn_model, detector_
             bboxes[cls_name].append([config.rpn_stride*x, config.rpn_stride*y, config.rpn_stride*(x+w), config.rpn_stride*(y+h)])
             probs[cls_name].append(np.max(P_cls[0, ii, :]))
    
-    print(f'Elapsed:{time.time()-start}')
+    # print(f'Elapsed:{time.time()-start}')
     return bboxes, probs
 
-def evaluate_model(rpn_model, detector_model, backbone, val_patch_list, class_list, metric_threshold, acceptance_treshold):
-
+def evaluate_model(rpn_model, detector_model, backbone, val_patch_list, class_list, map_threshold, acceptance_treshold):
     preds = {}
     mAP = []
     mPrec = []
-    mRecall = []
     val_datagen = prep.get_anchor_gt(config.TRAIN_PATCHES_FOLDER, val_patch_list, backbone=backbone, mode='eval', infinite_loop=False)
 
     for patch in val_datagen:
         image, _, _, _, _, patch_id = patch
-        bboxes, probs = get_predictions(image, class_list, acceptance_treshold=acceptance_treshold, rpn_model=rpn_model, detector_model=detector_model)
-        
-        # print(bboxes, probs)
-        
+        bboxes, probs = get_predictions(image, class_list, acceptance_treshold=acceptance_treshold, rpn_model=rpn_model, detector_model=detector_model)        
         detections = get_detections(patch_id, bboxes, probs)
-        macro_AP, macro_prec, macro_recall = get_img_scores(detections, patch_id, metric_threshold)
-        preds[patch_id] = {'bboxes':bboxes, 'probs':probs, 'mAP':macro_AP, 'macro_precision':macro_prec, 'macro_recall':macro_recall}
+        macro_AP, macro_prec = get_img_scores(detections, patch_id, map_threshold)
+        preds[patch_id] = {'bboxes':bboxes, 'probs':probs, 'mAP':macro_AP, 'macro_precision':macro_prec}#, 'macro_recall':macro_recall}
         mAP.append(macro_AP)
         mPrec.append(macro_prec)
-        mRecall.append(macro_recall)
-
+        
 
     total_mAP = np.array(mAP).mean()
     total_mPrec = np.array(mPrec).mean()
-    total_mRecall = np.array(mRecall).mean()
+    
+    print(f'\nTotal model metrics: mAP: {total_mAP} - mPrecision: {total_mPrec}')
+    return preds, total_mAP, total_mPrec
 
-    print(f'\nTotal model metrics: mAP: {total_mAP} - macro_precision: {total_mPrec} - macro_recall: {total_mRecall}')
-    return preds, total_mAP, total_mPrec, total_mRecall
-
-def compute_map(y_pred, gt_patch_id, data_folder):
+def compute_map(y_pred, gt_patch_id, data_folder, map_threshold):
     T = {}
     P = {}
     f = config.patch_dim / float(config.resizeFinalDim) if config.resizePatch else 1.
 
     gt = pd.read_pickle(f'{data_folder}/{gt_patch_id}/{gt_patch_id}.pkl')
+    # display(gt)
 
     gt['bbox_matched'] = False
     pred_probs = np.array(y_pred['prob'])
+    # print('714')
+    # print(pred_probs)
 
     box_idx_sorted_by_prob = np.argsort(pred_probs)[::-1]
 
@@ -738,12 +747,16 @@ def compute_map(y_pred, gt_patch_id, data_folder):
                 continue
             if gt_seen:
                 continue
-            gt_x1 = gt_box['x1s']/f
-            gt_x2 = gt_box['x2s']/f
-            gt_y1 = gt_box['y1s']/f
-            gt_y2 = gt_box['y2s']/f
+            gt_x1 = round(gt_box['x1s'])#/f
+            gt_x2 = round(gt_box['x2s'])#/f
+            gt_y1 = round(gt_box['y1s'])#/f
+            gt_y2 = round(gt_box['y2s'])#/f
+            # print('737')
+            # print((pred_x1, pred_y1, pred_x2, pred_y2), (gt_x1, gt_y1, gt_x2, gt_y2))
             iou = prep.iou((pred_x1, pred_y1, pred_x2, pred_y2), (gt_x1, gt_y1, gt_x2, gt_y2))
-            if iou >= 0.5:
+            # print('738')
+            # print(iou)
+            if iou >= map_threshold:
                 found_match = True
                 gt.at[idx,'bbox_matched'] = True
                 break
@@ -763,15 +776,14 @@ def compute_map(y_pred, gt_patch_id, data_folder):
 
     return T, P
 
-def get_img_scores(detections, patch_id, metric_threshold):
+def get_img_scores(detections, patch_id, map_threshold):
     T = {}
     P = {}
     P_tresh = {}
     all_aps = []
     all_prec = []
-    all_recall = []
     
-    t, p = compute_map(detections, patch_id, config.TRAIN_PATCHES_FOLDER)
+    t, p = compute_map(detections, patch_id, config.TRAIN_PATCHES_FOLDER, map_threshold)
     # print('762')
     # print(t, p)
 
@@ -787,25 +799,19 @@ def get_img_scores(detections, patch_id, metric_threshold):
         ap = average_precision_score(T[key], P[key])
         # print('773')
         # print(T[key], P[key])
-        P_tresh[key] = np.where(np.array(P[key]) > metric_threshold, 1, 0)
+        P_tresh[key] = np.where(np.array(P[key]) > 0.5, 1, 0)
 
         prec = precision_score(T[key], P_tresh[key], zero_division=0)
-        recall = recall_score(T[key], P_tresh[key], zero_division=0)
-        # print('{} AP: {}'.format(key, ap))
         all_aps.append(ap)
         all_prec.append(prec)
-        all_recall.append(recall)
-
+        
     mAP=np.mean(np.array(all_aps))
     macro_prec = np.mean(np.array(all_prec))
-    macro_recall =  np.mean(np.array(all_recall))
-
-    # print('mAP = {}'.format(mAP))
-    return mAP, macro_prec, macro_recall
+    
+    return mAP, macro_prec
 
 def get_model_last_checkpoint(backbone):
     try:
-    
         model_cp_dict = dict()
         model_dir = f'{config.MODEL_WEIGHTS}/{backbone}'
         print(f'Checking model checkpoints in directory {config.MODEL_WEIGHTS}/{backbone}')
@@ -833,17 +839,17 @@ def merge_dols(dol1, dol2):
     no = []
     return dict((k, dol1.get(k, no) + dol2.get(k, no)) for k in keys)
 
-def merge_dois(dol1, dol2):
+def merge_dois(doi1, doi2):
     """
-    Merges two dictionary of lists.
+    Merges two dictionary of integer.
     Example:
-        d = {'a': ['1','2'], 'b':['8']}
-        dd = {'a': ['3','4'], 'c': ['9']}
-        become -> {'a': ['1', '2', '3', '4'], 'b': ['8'], 'c': ['9']}
+        d = {'a': 10, 'b': 5}
+        dd = {'a': 3, 'c': 9}
+        become -> {'a': 13, 'b': 5, 'c': 9}
     """
-    keys = set(dol1).union(dol2)
+    keys = set(doi1).union(doi2)
     no = 0
-    return dict((k, dol1.get(k, no) + dol2.get(k, no)) for k in keys)
+    return dict((k, doi1.get(k, no) + doi2.get(k, no)) for k in keys)
 
 
 def moving_average(x, w):